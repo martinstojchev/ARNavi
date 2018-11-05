ff2214d7a281d7576c7b2f07172453d287411923
Martin <martinstojchev@hotmail.com>
2018-11-05 12:07:37 +0100

    Search friends ui added

--- ARNavi/Controller/FriendsVC.swift
+++ ARNavi/Controller/FriendsVC.swift
@@ -18,6 +18,9 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var friendsTableView: UITableView!
     
     var friendsData = [FriendsData]()
+    var friends = ["Mirko","Petar","Slave","Letka","Stojce","Leon","Boris"]
+    var filteredFriends = [FriendsData]()
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,23 +35,68 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         friendsData.append(FriendsData(image: UIImage(named: "profile_pic"), title: "Leon"))
         friendsData.append(FriendsData(image: UIImage(named: "profile_pic"), title: "Mirko"))
         friendsData.append(FriendsData(image: UIImage(named: "profile_pic"), title: "Boris"))
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Friends"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isFiltering() {
+            return filteredFriends.count
+        }
         return friendsData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        var cellTitle: String!
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell") as! FriendsCell
-        
+        if isFiltering(){
+         cellTitle = filteredFriends[indexPath.row].title!
+        }
+        else {
+         cellTitle = friendsData[indexPath.row].title!
+        }
         let cellImage = friendsData[indexPath.row].image!
-        let cellTitle = friendsData[indexPath.row].title!
+        
         
         cell.setFriendsCell(cellImage: cellImage, title: cellTitle)
         
         return cell
     }
     
+    // MARK: - Private instance methods
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredFriends = friendsData.filter({( friend : FriendsData) -> Bool in
+            return friend.title!.lowercased().contains(searchText.lowercased())
+        })
+        
+        friendsTableView.reloadData()
+    }
+    
 
+}
+
+
+extension FriendsVC: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+   
 }
