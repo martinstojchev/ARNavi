db49bc675e6c52bf78eb83bc37d8a40cbc9aefe4
Martin <martinstojchev@hotmail.com>
2018-11-05 12:14:10 +0100

    Search requests ui added

--- ARNavi/Controller/RequestsVC.swift
+++ ARNavi/Controller/RequestsVC.swift
@@ -20,7 +20,10 @@ class RequestsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var requestsTableView: UITableView!
     
     var data = [CellData]()
-
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var filteredRequests = [CellData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -36,19 +39,38 @@ class RequestsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         view.backgroundColor = AppColor.backgroundColor.rawValue
         requestsTableView.backgroundColor = AppColor.backgroundColor.rawValue
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Candies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isFiltering() {
+            return filteredRequests.count
+        }
+        
         return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "RequestCell") as! RequestsTableViewCell
+        var cellTitle: String!
         
         let cellImage = data[indexPath.row].image!
-        let cellTitle = data[indexPath.row].title!
+        if isFiltering(){
+          cellTitle = filteredRequests[indexPath.row].title!
+        }
+        else {
+          cellTitle = data[indexPath.row].title!
+        }
+        
         let acceptImage = data[indexPath.row].acceptImage!
         let declineImage = data[indexPath.row].declineImage!
         
@@ -56,6 +78,33 @@ class RequestsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         return cell
     }
+    
+    // MARK: - Private instance methods
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredRequests = data.filter({( request : CellData) -> Bool in
+            return request.title!.lowercased().contains(searchText.lowercased())
+        })
+        
+        requestsTableView.reloadData()
+    }
+    
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
 
 
+}
+extension RequestsVC: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
